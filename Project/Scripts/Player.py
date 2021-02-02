0231d9765f539504b72cb581a640fb28e8ed3b83
Erwan <erwan.morcet78@gmail.com>
2021-02-02 11:20:41 +0100

    Début de la class Player

---
+++ Project/Scripts/Player.py
@@ -0,0 +1,9 @@
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.max_health = 100
+        self.attack = 10
+        self.velocity = 5
+        self.image = pygame.image.load('') #mettre le chemin vers l'image du joueur
+        self.rect = self.image.get_rect()
