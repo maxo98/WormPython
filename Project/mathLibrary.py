aeb13dca41bfc39f5d9cee2b082c15d120cfe07d
Max <maxime.boidin.98@gmail.com>
2021-02-02 12:05:44 +0100

    projectile path with wind

--- Project/mathLibrary.py
+++ Project/mathLibrary.py
@@ -1,5 +1,6 @@
 import math
 
+
 # get the path of the projectile without wind
 def noWindPath(pos, speed, angle, time, gravity):
     v0 = speed[0] * math.cos(angle)
@@ -9,4 +10,11 @@ def noWindPath(pos, speed, angle, time, gravity):
     currentPos = [xt, yt]
     return currentPos
 
-def windPath(pos, speed, angle, time, gravity):
+
+# get the path of the projectile with wind
+def windPath(pos, speed, angle, time, friction, gravity):
+    xt = pos[0] + ((speed[0] * math.cos(angle)) / friction) * (1 - math.exp(-friction * time))
+    yt = pos[1] + ((speed[1] * math.sin(angle)) / friction) * (1 - math.exp(- friction * time)) - (
+            (gravity * time) / friction)
+    currentPos = [xt, yt]
+    return currentPos
--- Project/player.py
+++ Project/player.py
@@ -1,5 +1,7 @@
 import pygame
+
 pygame.init()
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -8,12 +10,10 @@ class Player(pygame.sprite.Sprite):
         self.max_health = 100
         self.attack = 10
         self.velocity = 5
-        self.image = pygame.image.load('') #mettre le chemin vers l'image du joueur
+        self.image = pygame.image.load('')  # mettre le chemin vers l'image du joueur
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = 500
 
-
-
-#Dans la boucle tant que après avoir appliqué la fenetre du jeu :
-#appliquer l'image du joueur : screen.blit(game.player.image, game.player.rect)
+# Dans la boucle tant que après avoir appliqué la fenetre du jeu :
+# appliquer l'image du joueur : screen.blit(game.player.image, game.player.rect)
