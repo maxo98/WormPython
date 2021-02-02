cf02e3870dba6b620b9824612b235547c6e6a6ae
Erwan <erwan.morcet78@gmail.com>
2021-02-02 11:55:07 +0100

    class Game qui initie le joueur et non le main directement

---
+++ Project/Game.py
@@ -0,0 +1,14 @@
+import pygame
+from player import Player
+
+class Game:
+    def __init__(self):
+        #générer le joueur
+        self.player = Player()
+
+
+
+
+#Au top de main.py: from game import Game
+#Après avoir chargé l'arrière plan:
+#game = Game()
--- Project/Player.py
+++ Project/Player.py
@@ -1,5 +1,5 @@
 import pygame
-
+pygame.init()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -8,5 +8,12 @@ class Player(pygame.sprite.Sprite):
         self.max_health = 100
         self.attack = 10
         self.velocity = 5
-        self.image = pygame.image.load('')
+        self.image = pygame.image.load('') #mettre le chemin vers l'image du joueur
         self.rect = self.image.get_rect()
+        self.rect.x = 400
+        self.rect.y = 500
+
+
+
+#Dans la boucle tant que après avoir appliqué la fenetre du jeu :
+#appliquer l'image du joueur : screen.blit(game.player.image, game.player.rect)
